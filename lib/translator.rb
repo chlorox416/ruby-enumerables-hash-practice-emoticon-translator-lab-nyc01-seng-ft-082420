868247f12a586f192bef465f24db548d01a30cc7
C. Chang <chloechang416@gmail.com>
2020-08-22 00:49:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,19 @@
 # require modules here
 
-def load_library(file_path)
-  # code goes here
-  emoticons = YAML.load_file(file_path)
-   new_hash = {}
-    emoticons.each do |key,value|
-   new_hash[key] = {}
- new_hash[key][:english] = value[0]
- new_hash[key][:japanese] = value[1]
- end
- new_hash
+def load_library
+  emoticons = YAML.load_file
+  new_hash = {}
 end
+
+
+
 
 def get_japanese_emoticon
   # code goes here
 end
+
+
+
 
 def get_english_meaning
   # code goes here
